c826f319fcf47777ad4242ed0fa60b796262f61f
Oscar Esteban <code@oscaresteban.es>
2018-12-09 21:45:41 -0800

    add one doctest

---
+++ smriprep/utils/bids.py
@@ -0,0 +1,85 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Utilities to handle BIDS inputs
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+"""
+
+def write_derivative_description(bids_dir, deriv_dir):
+    """Write a ``dataset_description.json`` for the derivatives
+    folder.
+
+    .. testsetup::
+
+    >>> from pathlib import Path
+    >>> from inspect import getfile, currentframe
+    >>> from tempfile import TemporaryDirectory
+    >>> tmpdir = TemporaryDirectory()
+    >>> root_dir = Path(getfile(currentframe())).resolve().parent
+    >>> bids_dir = root_dir / 'data' / 'tests'
+    >>> deriv_desc = Path(tmpdir.name) / 'dataset_description.json'
+
+    .. doctest::
+
+    >>> write_derivative_description(bids_dir, deriv_desc.parent)
+    >>> deriv_desc.is_file()
+    True
+
+    .. testcleanup::
+
+    >>> tmpdir.cleanup()
+
+
+    """
+    import os
+    from pathlib import Path
+    import json
+    from ..__about__ import __version__, __url__, DOWNLOAD_URL
+
+    bids_dir = Path(bids_dir)
+    deriv_dir = Path(deriv_dir)
+    desc = {
+        'Name': 'sMRIPrep - Structural MRI PREProcessing workflow',
+        'BIDSVersion': '1.1.1',
+        'PipelineDescription': {
+            'Name': 'sMRIPrep',
+            'Version': __version__,
+            'CodeURL': DOWNLOAD_URL,
+            },
+        'CodeURL': __url__,
+        'HowToAcknowledge':
+            'Please cite our paper (https://doi.org/10.1101/306951), and '
+            'include the generated citation boilerplate within the Methods '
+            'section of the text.',
+        }
+
+    # Keys that can only be set by environment
+    if 'SMRIPREP_DOCKER_TAG' in os.environ:
+        desc['DockerHubContainerTag'] = os.environ['SMRIPREP_DOCKER_TAG']
+    if 'SMRIPREP_SINGULARITY_URL' in os.environ:
+        singularity_url = os.environ['SMRIPREP_SINGULARITY_URL']
+        desc['SingularityContainerURL'] = singularity_url
+
+        singularity_md5 = _get_shub_version(singularity_url)
+        if singularity_md5 is not None and singularity_md5 is not NotImplemented:
+            desc['SingularityContainerMD5'] = _get_shub_version(singularity_url)
+
+    # Keys deriving from source dataset
+    orig_desc = {}
+    fname = bids_dir / 'dataset_description.json'
+    if fname.exists():
+        with fname.open() as fobj:
+            orig_desc = json.load(fobj)
+
+    if 'DatasetDOI' in orig_desc:
+        desc['SourceDatasetsURLs'] = ['https://doi.org/{}'.format(
+            orig_desc['DatasetDOI'])]
+    if 'License' in orig_desc:
+        desc['License'] = orig_desc['License']
+
+    with (deriv_dir / 'dataset_description.json').open('w') as fobj:
+        json.dump(desc, fobj, indent=4)
+
+
+def _get_shub_version(singularity_url):
+    return NotImplemented
