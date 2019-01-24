c3bffb4ec37070f9bdf116061647f9b7e354d0fb
Oscar Esteban <code@oscaresteban.es>
2019-01-23 21:07:31 -0800

    pin niworkflows 0.6.1

--- smriprep/__about__.py
+++ smriprep/__about__.py
@@ -51,7 +51,7 @@ REQUIRES = [
     'matplotlib>=2.2.0',
     'nibabel>=2.2.1',
     'nipype>=1.1.6',
-    'niworkflows',
+    'niworkflows>=0.6.1,<0.7',
     'numpy',
     'packaging',
     'pybids==0.6.5',
@@ -61,8 +61,6 @@ REQUIRES = [
 
 
 LINKS_REQUIRES = [
-    'git+https://github.com/poldracklab/niworkflows.git@'
-    '140ca5f3c9a5e5599ad621b193dfc34c70b096a4#niworkflows-0.5.3',
 ]
 
 TESTS_REQUIRES = [
