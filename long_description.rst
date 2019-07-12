c14b6ae270218f533294ea1163035afd06be97b7
Christopher J. Markiewicz <markiewicz@stanford.edu>
2019-07-12 09:37:49 -0400

    MAINT: Move to setup.cfg + pyproject.toml

---
+++ long_description.rst
@@ -0,0 +1,12 @@
+The workflow is based on `Nipype <https://nipype.readthedocs.io>`_ and encompases a large
+set of tools from well-known neuroimaging packages, including
+`FSL <https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/>`_,
+`ANTs <https://stnava.github.io/ANTs/>`_,
+`FreeSurfer <https://surfer.nmr.mgh.harvard.edu/>`_,
+`AFNI <https://afni.nimh.nih.gov/>`_,
+and `Nilearn <https://nilearn.github.io/>`_.
+This pipeline was designed to provide the best software implementation for each state of
+preprocessing, and will be updated as newer and better neuroimaging software becomes
+available.
+
+Support is provided on `neurostars.org <https://neurostars.org/tags/smriprep>`_.
--- setup.py
+++ setup.py
@@ -1,88 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Author: oesteban
-# @Date:   2015-11-19 16:44:27
-""" smriprep setup script """
+import sys
+from setuptools import setup
+import versioneer
 
 
-def main():
-    """ Install entry-point """
-    from pathlib import Path
-    from inspect import getfile, currentframe
-    from setuptools import setup, find_packages
-    from smriprep.__about__ import (
-        __package__,
-        __version__,
-        __author__,
-        __email__,
-        __maintainer__,
-        __license__,
-        __description__,
-        __longdesc__,
-        __url__,
-        DOWNLOAD_URL,
-        CLASSIFIERS,
-        REQUIRES,
-        SETUP_REQUIRES,
-        LINKS_REQUIRES,
-        TESTS_REQUIRES,
-        EXTRA_REQUIRES,
-    )
-
-    pkg_data = {
-        __package__: [
-            'data/*.json',
-            'data/*.nii.gz',
-            'data/*.mat',
-            'data/boilerplate.bib',
-            'data/itkIdentityTransform.txt',
-            'data/reports/config.json',
-            'data/reports/report.tpl',
-        ]
-    }
-
-    version = None
-    cmdclass = {}
-
-    root_dir = Path(getfile(currentframe())).resolve().parent
-    verfile = root_dir / __package__ / 'VERSION'
-    if verfile.is_file():
-        version = verfile.read_text().splitlines()[0].strip()
-        pkg_data[__package__].insert(0, 'VERSION')
-
-    if version is None:
-        import versioneer
-        version = versioneer.get_version()
-        cmdclass = versioneer.get_cmdclass()
-
-    setup(
-        name=__package__,
-        version=__version__,
-        description=__description__,
-        long_description=__longdesc__,
-        author=__author__,
-        author_email=__email__,
-        maintainer=__maintainer__,
-        maintainer_email=__email__,
-        url=__url__,
-        license=__license__,
-        classifiers=CLASSIFIERS,
-        download_url=DOWNLOAD_URL,
-        # Dependencies handling
-        setup_requires=SETUP_REQUIRES,
-        install_requires=REQUIRES,
-        tests_require=TESTS_REQUIRES,
-        extras_require=EXTRA_REQUIRES,
-        dependency_links=LINKS_REQUIRES,
-        package_data=pkg_data,
-        entry_points={'console_scripts': [
-            'smriprep=smriprep.cli.run:main',
-        ]},
-        packages=find_packages(exclude=("tests",)),
-        zip_safe=False,
-        cmdclass=cmdclass,
-    )
+# Give setuptools a hint to complain if it's too old a version
+# 30.3.0 allows us to put most metadata in setup.cfg
+# Should match pyproject.toml
+# Not going to help us much without numpy or new pip, but gives us a shot
+SETUP_REQUIRES = ['setuptools >= 30.3.0']
+# This enables setuptools to install wheel on-the-fly
+SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
 
 
 if __name__ == '__main__':
-    main()
+    setup(name='smriprep',
+          version=versioneer.get_version(),
+          cmdclass=versioneer.get_cmdclass(),
+          setup_requires=SETUP_REQUIRES,
+          )
