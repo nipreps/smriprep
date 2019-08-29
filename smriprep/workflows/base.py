ee240735bd0488dae2b7871f6c45206248016fa0
Ross Blair <rosswilsonblair@gmail.com>
2019-08-29 09:54:48 -0500

    modernize pybids import

--- smriprep/workflows/base.py
+++ smriprep/workflows/base.py
@@ -61,7 +61,7 @@ def init_smriprep_wf(
 
         import os
         from collections import OrderedDict
-        from pybids import BIDSLayout
+        from bids import BIDSLayout
         os.environ['FREESURFER_HOME'] = os.getcwd()
         from smriprep.workflows.base import init_smriprep_wf
         wf = init_smriprep_wf(
