68ec087a2acb9f7291fb444e5499767d293b7251
Ross Blair <rosswilsonblair@gmail.com>
2019-10-01 12:26:53 -0500

    have init_anat_norm_wf doc argument match actual argument name

--- smriprep/workflows/norm.py
+++ smriprep/workflows/norm.py
@@ -36,7 +36,7 @@ def init_anat_norm_wf(
         wf = init_anat_norm_wf(
             debug=False,
             omp_nthreads=1,
-            template_list=['MNI152NLin2009cAsym', 'MNI152NLin6Asym'],
+            templates=['MNI152NLin2009cAsym', 'MNI152NLin6Asym'],
         )
 
     **Parameters**
