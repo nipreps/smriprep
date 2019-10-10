cecd376846ce995e512bd5d4b971e7a9647201a5
delavega4 <aleph4@gmail.com>
2019-10-10 17:36:24 -0500

    Binarize in/outs

--- smriprep/workflows/anatomical.py
+++ smriprep/workflows/anatomical.py
@@ -162,8 +162,8 @@ N4BiasFieldCorrection.""" % _ants_version, DeprecationWarning)
 
     wf.connect([
         (inputnode, inu_n4_final, [('in_files', 'input_image')]),
-        (inputnode, thr_brainmask, [(('in_files', _pop), 'input_image')]),
-        (thr_brainmask, outputnode, [('output_image', 'out_mask')]),
+        (inputnode, thr_brainmask, [(('in_files', _pop), 'in_file')]),
+        (thr_brainmask, outputnode, [('out_file', 'out_mask')]),
         (inu_n4_final, copy_xform, [('output_image', 'out_file')]),
         (inu_n4_final, copy_xform, [('bias_image', 'bias_image')]),
         (copy_xform, outputnode, [('out_file', 'out_file')]),
