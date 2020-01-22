997c1e591e822c0217f8d42a5cf49bb7551824b1
mathiasg <mathiasg@stanford.edu>
2020-01-22 13:00:15 -0500

    tst: fix most output space parsing tests

--- smriprep/cli/tests/test_utils.py
+++ smriprep/cli/tests/test_utils.py
@@ -15,13 +15,15 @@ def test_output_spaces(monkeypatch):
     """Check the --output-spaces argument parser."""
     with monkeypatch.context() as m:
         m.setattr(u, '_TF_TEMPLATES', TEST_TEMPLATES)
-        assert u.output_space('MNI152NLin2009cAsym') == ('MNI152NLin2009cAsym', {})
+        assert u.output_space('MNI152NLin2009cAsym') == [('MNI152NLin2009cAsym', {})]
         assert u.output_space('MNI152NLin2009cAsym:native') == \
-            ('MNI152NLin2009cAsym', {'native': True})
+            [('MNI152NLin2009cAsym', {'native': True})]
         assert u.output_space('MNI152NLin2009cAsym:res-2') == \
-            ('MNI152NLin2009cAsym', {'res': '2'})
+            [('MNI152NLin2009cAsym', {'res': '2'})]
+        assert u.output_space('MNI152:res-1:res-2') == \
+            [('MNI152', {'res': '1'}), ('MNI152', {'res': '2'})]
         assert u.output_space('MNIInfant:res-2:cohort-1') == \
-            ('MNIInfant', {'res': '2', 'cohort': '1'})
+            [('MNIInfant', {'res': '2', 'cohort': '1'})]
 
         with pytest.raises(ValueError):
             u.output_space('UnkownTemplate')
@@ -32,13 +34,12 @@ def test_template_parser(monkeypatch):
     with monkeypatch.context() as m:
         m.setattr(u, '_TF_TEMPLATES', TEST_TEMPLATES)
 
-        assert list(u._template(['MNI152NLin2009cAsym']).keys()) == ['MNI152NLin2009cAsym']
+        assert u._template(['MNI152NLin2009cAsym']) == [('MNI152NLin2009cAsym', {})]
         assert u._template(['MNI152NLin2009cAsym', 'MNI152NLin2009cAsym:res-2']) == \
-            OrderedDict([('MNI152NLin2009cAsym', {})])
+            [('MNI152NLin2009cAsym', {}), ('MNI152NLin2009cAsym', {'res': '2'})]
 
-        assert u._template(['MNI152NLin2009cAsym', 'MNI152NLin2009cAsym:res-2',
-                            'MNI152NLin6Asym:res-2', 'MNI152NLin6Asym']) == \
-            OrderedDict([('MNI152NLin2009cAsym', {}), ('MNI152NLin6Asym', {'res': '2'})])
+        assert u._template(['MNI152NLin2009cAsym:res-1', 'MNI152NLin6Asym:res-2']) == \
+            [('MNI152NLin2009cAsym', {'res': '1'}), ('MNI152NLin6Asym', {'res': '2'})]
 
         with pytest.raises(ValueError):
             u._template(['MNI152NLin6Asym:res-2', 'UnkownTemplate:res-2'])
@@ -47,13 +48,12 @@ def test_template_parser(monkeypatch):
             u._template(['MNI152NLin6Asym:res-2', 'func'])
 
         assert u._template(['MNI152NLin6Asym:res-2', 'fsnative']) == \
-            OrderedDict([('MNI152NLin6Asym', {'res': '2'}), ('fsnative', {})])
+            [('MNI152NLin6Asym', {'res': '2'}), ('fsnative', {})]
 
         u.ParseTemplates.set_nonstandard_spaces('func')
         assert u._template(['MNI152NLin2009cAsym:res-2', 'func']) == \
-            OrderedDict([('MNI152NLin2009cAsym', {'res': '2'}), ('func', {})])
+           [('MNI152NLin2009cAsym', {'res': '2'}), ('func', {})]
 
         u.ParseTemplates.set_nonstandard_spaces(['func', 'fsnative'])
         assert u._template(['MNI152NLin2009cAsym:res-2', 'func', 'fsnative']) == \
-            OrderedDict([('MNI152NLin2009cAsym', {'res': '2'}),
-                         ('func', {}), ('fsnative', {})])
+            [('MNI152NLin2009cAsym', {'res': '2'}), ('func', {}), ('fsnative', {})]
