2fd44b07314a9bf2f0d9ef473015a1755850d263
mathiasg <mathiasg@stanford.edu>
2020-06-12 11:45:29 -0400

    STY: pep8 fixes

--- smriprep/utils/tests/test_misc.py
+++ smriprep/utils/tests/test_misc.py
@@ -2,7 +2,8 @@ import pytest
 
 from ..misc import fs_isRunning
 
-def gen_fs_dir(tmp_path, isrunning):
+
+def _gen_fsdir(tmp_path, isrunning):
     fsdir = tmp_path / 'sample_freesurfer'
     (fsdir / 'sub-01' / 'scripts').mkdir(parents=True)
     (fsdir / 'sub-01' / 'scripts' / 'recon-all.log').write_text('log')
@@ -11,13 +12,14 @@ def gen_fs_dir(tmp_path, isrunning):
         (fsdir / 'sub-01' / 'scripts' / 'IsRunning.lh').write_text('running')
     return fsdir
 
+
 @pytest.mark.parametrize('isrunning,mtime_tol,error', [
     (False, 86400, None),
     (True, 86400, RuntimeError),
     (True, 0, None),
 ])
 def test_fs_isRunning(tmp_path, isrunning, mtime_tol, error):
-    fs_dir = gen_fs_dir(tmp_path, isrunning)
+    fs_dir = _gen_fsdir(tmp_path, isrunning)
     if error is None:
         fs_isRunning(fs_dir, 'sub-01', mtime_tol=mtime_tol)
         assert not tuple(fs_dir.glob('**/IsRunning*'))
