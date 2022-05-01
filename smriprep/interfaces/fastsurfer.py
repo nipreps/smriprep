aa49b700cfcaf34e899f14f5883d90ef6c124d48
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2022-05-01 13:42:47 -0500

    Switch fs_license to use FS_LICENSE os env var

--- smriprep/interfaces/fastsurfer.py
+++ smriprep/interfaces/fastsurfer.py
@@ -142,7 +142,7 @@ class FastSInputSpec(CommandLineInputSpec):
         desc="T1 full head input (not bias corrected, global path)"
     )
     fs_license = File(
-        "/opt/freesurfer/license.txt",
+        os.environ["FS_LICENSE"],
         exists=True,
         mandatory=True,
         argstr="--fs_license %s",
@@ -316,7 +316,7 @@ class FastSurfSourceInputSpec(BaseInterfaceInputSpec):
         desc="T1 full head input (not bias corrected, global path)"
     )
     fs_license = File(
-        "/opt/freesurfer/license.txt",
+        os.environ["FS_LICENSE"],
         exists=True,
         mandatory=True,
         argstr="--fs_license %s",
