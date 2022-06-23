0b20fdd24bd4b517dce12a62462cbec456e45e57
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2022-06-23 14:07:22 -0500

    Fix PEP8 compliance

--- smriprep/interfaces/fastsurfer.py
+++ smriprep/interfaces/fastsurfer.py
@@ -314,6 +314,7 @@ class FastSurfSourceInputSpec(BaseInterfaceInputSpec):
         desc="T1 full head input (not bias corrected, global path)"
     )
 
+
 class FastSurfSourceOutputSpec(TraitedSpec):
     T1 = File(
         exists=True,
