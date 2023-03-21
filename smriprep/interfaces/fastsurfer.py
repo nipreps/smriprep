b6f083a0a756532841ad4faf083f7bc5acc53d77
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2023-03-21 16:45:43 -0500

    Debugging FastSurfer Workflow

--- smriprep/interfaces/fastsurfer.py
+++ smriprep/interfaces/fastsurfer.py
@@ -3,7 +3,7 @@
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """
 The FastSufer module provides basic functions
-for running FastSurfer CNN and surface processing.
+for running FastSurfer VINN and surface processing.
 
 """
 from nipype.interfaces.base import (
