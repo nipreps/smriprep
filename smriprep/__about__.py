700a64a355af7e4e6d4bdab93410cafc32fc70a0
mathiasg <mathiasg@stanford.edu>
2023-08-25 11:19:22 -0400

    MAINT: Add version fallback if not available

--- smriprep/__about__.py
+++ smriprep/__about__.py
@@ -24,7 +24,10 @@
 Base module variables
 """
 
-from ._version import __version__
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0+unknown"
 
 __copyright__ = "Copyright 2019, Center for Reproducible Neuroscience, Stanford University"
 __credits__ = [
