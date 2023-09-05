1512c62dd3c9c9561ad2ad2ac6cf23e1d101a60a
mathiasg <mathiasg@stanford.edu>
2023-09-05 10:26:38 -0400

    TEST: Add to testing namespace

--- smriprep/conftest.py
+++ smriprep/conftest.py
@@ -1,3 +1,16 @@
 import os
 
+import pytest
+import numpy
+
+from smriprep.data import load_resource
+
 os.environ['NO_ET'] = '1'
+
+
+@pytest.fixture(autouse=True)
+def populate_namespace(doctest_namespace, tmp_path):
+    doctest_namespace['os'] = os
+    doctest_namespace['np'] = numpy
+    doctest_namespace['load'] = load_resource
+    doctest_namespace['testdir'] = tmp_path
