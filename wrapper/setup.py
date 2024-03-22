2ba6c421041aab00be5b1748d4026d097e48fddc
Chris Markiewicz <effigies@gmail.com>
2024-03-22 15:07:58 -0400

    PY27: Ensure setup.py runs on older build tools

--- wrapper/setup.py
+++ wrapper/setup.py
@@ -13,28 +13,31 @@ SETUP_REQUIRES = ['setuptools >= 30.3.0']
 # This enables setuptools to install wheel on-the-fly
 SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
 
-
-VERSION = get_version(root='..', relative_to=__file__)
+try:
+    VERSION = get_version(root='..', relative_to=__file__)
+except LookupError:  # PY27 will not be able to build with a correct version, probably
+    # Note that `python setup.py *` will work, but `pip install` generally won't
+    VERSION = '99.99.99'
 
 
 def update_version(target_file, version):
     with open(target_file) as fp:
         contents = fp.read()
-    updated = contents.replace('__version__ = "99.99.99"', '__version__ = "{%s}"' % version)
+    updated = contents.replace('__version__ = "99.99.99"', '__version__ = "%s"' % version)
     with open(target_file, 'w') as fp:
         fp.write(updated)
 
 
 class PatchVersionSdist(sdist):
     def make_release_tree(self, base_dir, files):
-        super().make_release_tree(base_dir, files)
+        sdist.make_release_tree(self, base_dir, files)
         target_file = base_dir + '/smriprep_docker.py'
         update_version(target_file, VERSION)
 
 
 class PatchVersionBuild(build_py):
     def run(self):
-        super().run()
+        build_py.run(self)
         target_file = self.build_lib + '/smriprep_docker.py'
         update_version(target_file, VERSION)
 
