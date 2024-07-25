0e1b7cbde251ffa44d4a98120f71b634b9fcc79f
mathiasg <mathiasg@stanford.edu>
2024-07-25 14:51:26 -0400

    DOC: Modify function signature

--- smriprep/workflows/anatomical.py
+++ smriprep/workflows/anatomical.py
@@ -1378,7 +1378,7 @@ def init_anat_template_wf(
 
             from smriprep.workflows.anatomical import init_anat_template_wf
             wf = init_anat_template_wf(
-                longitudinal=False, omp_nthreads=1, num_files=1, contrast="T1w"
+                longitudinal=False, omp_nthreads=1, num_files=1, image_type="T1w"
             )
 
     Parameters
