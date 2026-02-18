2be6981f0fdfca07208931bef3f16158a3f73018
Oscar Esteban <code@oscaresteban.es>
2026-02-18 14:54:03 +0100

    CHORE: Normalize license header (#552)
    * chore: add license header to all files and remove copyright year if present
    
    * sty: empty files do not need header, canary test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: allow empty files to remain empty (i.e., do not force header)
    
    * fix: issues in headers script
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/smriprep/workflows/outputs.py
+++ src/smriprep/workflows/outputs.py
@@ -1,7 +1,7 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 #
-# Copyright 2021 The NiPreps Developers <nipreps@gmail.com>
+# Copyright The NiPreps Developers <nipreps@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
